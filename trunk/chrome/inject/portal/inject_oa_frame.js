4b2ccea75a2fb022675d2fa8adec4b9b9463534a
yaoancheng@gmail.com <yaoancheng@gmail.com@b5ad9fc9-fe54-84cf-8168-24ff7e8a642f>
2011-12-19 15:51:04 +0000

    已经利用设置登录，修改了登录的逻辑和错误

---
+++ trunk/chrome/inject/portal/inject_oa_frame.js
@@ -0,0 +1,16 @@
+/**
+ * portal中首页有不少添加的空白位置，用于占位，在chrome中显示错位
+ <tr style="height:8px;"><td></td></tr>
+
+ */
+( function init() {
+	window.setTimeout(function() {
+		var trs = document.getElementsByTagName("tr"), tr;
+		for(var i in trs) {
+			tr = trs[i];
+			if(tr.style && tr.style.height == "8px") {
+				tr.parentElement.removeChild(tr);
+			}
+		}
+	}, 1000);
+}());
