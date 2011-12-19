dbfa708e8336b07ed37bfa0fa81c1135be3666b5
yaoancheng@gmail.com <yaoancheng@gmail.com@b5ad9fc9-fe54-84cf-8168-24ff7e8a642f>
2011-12-19 15:51:04 +0000

    已经利用设置登录，修改了登录的逻辑和错误

--- chrome/inject/portal/inject_oa_ad.js
+++ chrome/inject/portal/inject_oa_ad.js
@@ -1,7 +1,63 @@
 /**
- * 结局
+ * 广告内容
  */
-
 
 //http://ad.services.gmcc.net//Modules/ADUrlPage/RollAdPage.aspx?nid=a24224cf-b278-4361-995e-f33019106ab2&sysid=&sid=16&adId=26a5680d-b8fd-48a5-bd2d-d24883509fd4
 
+/**
+ * 1 屏蔽中间弹窗;先使用折中的办法转向空白页
+ */
+(function init() {
+
+	//TODO: 临时停用
+	return;
+
+	var target = "http://ad.services.gmcc.net//Modules/ADUrlPage/RollAdPage.aspx";
+	var src = window.location.href;
+
+	if(src.indexOf(target) == -1)
+		return;
+
+	// window.location.href= "about:blank";
+	// return;
+	// alert(document.body.innerHTML);
+	var bodyContent = document.body.innerHTML;
+	var headContent = document.head.innerHTML;
+
+	var headElement = document.getElementsByTagName("head")[0];
+
+	var links = headElement.getElementsByTagName("link");
+	var scripts = headElement.getElementsByTagName("script");
+
+	var linkHrefs = [], scriptSrc = [], scriptContent = [];
+	for(var i in links) {
+		if(links[i].href)
+			linkHrefs.push(links[i].href);
+	}
+
+	for(var i in scripts) {
+		if(scripts[i].src)
+			scriptSrc.push(scripts[i].src);
+		else
+			scriptContent.push(scripts[i].innerHTML);
+	}
+
+	//这句话会覆盖body和head的所有内容
+	document.writeln(bodyContent);
+	headElement = document.getElementsByTagName("head")[0];
+
+	for(var i in linkHrefs) {
+		window.loadjscssfile(linkHrefs[i], "css");
+	}
+
+	for(var i in scriptSrc) {
+		window.loadjscssfile(scriptSrc[i], "js");
+	}
+
+	for(var i in scriptContent) {
+		window.loadjscssfile(scriptContent[i]);
+	}
+
+	return;
+
+})();
--- chrome/inject/portal/inject_portal_login.js
+++ chrome/inject/portal/inject_portal_login.js
@@ -3,22 +3,33 @@
  */
 
 $(function() {
-	
-	
-	form = $("<form></form>");
-	form.attr('action', 'http://portal.gmcc.net/amserver/UI/Login');
-	form.attr('method', 'post');
-	input1 = $("<input type='text' name='Login.Token1' value='chengyaoan' />");
-	input2 = $("<input type='text' name='Login.Token2' value='cheng12#$' />");
-	input3 = $("<input type='text' name='module' value='LDAP' />")
-	input4 = $("<input type='text' name='goto' value='http://contacts.services.gmcc.net/UserInfoSimple.aspx?nid=61ca46b7-0b76-41ae-9e1a-cd9adba8c86c&sid=11&f=app' />")
-	form.append(input1);
-	form.append(input2);
-	form.append(input3);
-	form.append(input4);
-	form.appendTo("body");
-	form.css('display', 'none');
-	form.submit();
-	
-	window.log("自动登录Portal...");
+	chrome.extension.sendRequest({
+		cmd : "setting",
+		args : ["PortalName", "PortalPwd"]
+	}, function(response) {
+		if(response.result) {
+			form = $("<form></form>");
+			form.attr('action', 'https://pac-am.gmcc.net/amserver/UI/Login');
+			form.attr('method', 'post');
+			input1 = $("<input type='text' name='Login.Token1' value='" + response[0] + "' />");
+			input2 = $("<input type='text' name='Login.Token2' value='" + response[1] + "' />");
+			input3 = $("<input type='text' name='module' value='LDAP' />")
+			input4 = $("<input type='text' name='goto' value='http://contacts.services.gmcc.net/UserInfoSimple.aspx?nid=61ca46b7-0b76-41ae-9e1a-cd9adba8c86c&sid=11&f=app' />")
+			form.append(input1);
+			form.append(input2);
+			form.append(input3);
+			form.append(input4);
+			form.appendTo("body");
+			form.css('display', 'none');
+			form.submit();
+
+			window.log("自动登录Portal...");
+			return;
+		}
+
+		window.err("无法自动登录");
+		return;
+
+		// response.result;
+	});
 })
