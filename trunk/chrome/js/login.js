af982647f045f8369781a0bd8f7f8ecc3ddb0aa3
yaoancheng@gmail.com <yaoancheng@gmail.com@b5ad9fc9-fe54-84cf-8168-24ff7e8a642f>
2012-01-21 03:22:51 +0000

    离线通讯录功能基本完善

--- trunk/chrome/js/login.js
+++ trunk/chrome/js/login.js
@@ -1,46 +1,51 @@
 //登录OA的库，需要用到Jquery
 
-window.testLogin = function(callback) {
-	$.ajax({
-		type : "GET",
-		url : "http://pac.gmcc.net/portal/dt",
-		success : function(msg) {
-			callback = callback ? callback : function(result) {alert(result)
-			};
-			if(msg.match(/redirectToDesktop/g)) {
+window.Login = {
+	buildUrl : function(name, pwd) {
+		var loginUrl = "https://pac-am.gmcc.net/amserver/UI/Login?goto=http://pac.gmcc.net/portal/dt?provider=DISHI_Portal";
+		loginUrl += "&Login.Token1=" + name;
+		loginUrl += "&Login.Token2=" + pwd;
+		loginUrl += "&module=LDAP";
+		return loginUrl;
+	},
+	testLogin : function(callback) {
+		$.ajax({
+			type : "GET",
+			url : "http://pac.gmcc.net/portal/dt",
+			success : function(msg) {
+				callback = callback ? callback : function(result) {alert(result)
+				};
+				if(msg.match(/redirectToDesktop/g)) {
+					callback(false);
+				} else {
+					callback(true)
+				}
+			},
+			error : function() {
 				callback(false);
-			} else {
-				callback(true)
 			}
-		},
-		error : function() {
-			callback(false);
-		}
-	});
-}
-
-window.login = function(name,pwd,callback) {
-
-	var loginUrl = "https://pac-am.gmcc.net/amserver/UI/Login?goto=http://pac.gmcc.net/portal/dt?provider=DISHI_Portal";
-	loginUrl += "&Login.Token1=" + name;
-	loginUrl += "&Login.Token2=" + pwd;
-	loginUrl += "&module=LDAP";
-	// alert(loginUrl);
-	var divBackApp = document.getElementById("divBackApp");
-	if(!divBackApp) {
-		$("body").append('<div id="divBackApp" style="position: absolute;left:-10px;"></div>');
-	}
-	callback = callback ? callback : function() {alert("已登录！");
-	};
-	var frLogin = $("<iframe id='frLogin' src='" + loginUrl + "' style='width:1px;height;1px;position:absolute;'/>").load(function() {
-		testLogin(function(result) {
-			if(result) {
-				$("#divBackApp").empty();
-				callback(true);
-			} else
-				callback(false);
 		});
-	});
 
-	$("#divBackApp").append(frLogin);
-}
+	},
+	login : function(name, pwd, callback) {
+		var loginUrl = Login.buildUrl(name, pwd);
+		// alert(loginUrl);
+		var divBackApp = document.getElementById("divBackApp");
+		if(!divBackApp) {
+			$("body").append('<div id="divBackApp" style="position: absolute;left:-10px;"></div>');
+		}
+		callback = callback ? callback : function() {alert("已登录！");
+		};
+		var frLogin = $("<iframe id='frLogin' src='" + loginUrl + "' style='width:1px;height;1px;position:absolute;'/>").load(function() {
+			Login.testLogin(function(result) {
+				if(result) {
+					$("#divBackApp").empty();
+					callback(true);
+				} else
+					callback(false);
+			});
+		});
+
+		$("#divBackApp").append(frLogin);
+	}
+};
---
+++ trunk/chrome/js/logj.js
@@ -0,0 +1,60 @@
+//基本类库，提供提示的基本功能，与log.js不同，需要jquery支持
+
+window.Log = {
+	/**
+	 * log 提示用户信息
+	 */
+	log : function(m, color) {
+		$("#messageBox").show();
+		color = color ? color : "#68af02";
+		var messageBox = document.getElementById("messageBox"), message = document.getElementById("message");
+		messageBox.style.filter = "alpha(opacity=100)";
+		messageBox.style.visibility = "visible";
+		messageBox.style.zIndex = "1000";
+		messageBox.style.left = "10px";
+
+		message.style.visibility = "visible";
+		//录入内容
+		var strHtml = "<div style='background:" + color + "; padding:3px 10px; height:15px; color:#FFFFFF; font-size:13px;'><b>" + m + "</b></div>";
+		message.innerHTML = strHtml;
+		
+		//调用关闭的时间
+		$("#messageBox").fadeOut(2000);		
+	},
+	/**
+	 * err 显示错误提示
+	 */
+	err : function(m) {
+		log(m, "#ef8f00");
+	},
+	/**
+	 * 初始化
+	 */
+	_init : function() {
+		var messageBox = document.createElement("div");
+		messageBox.id = "messageBox";
+		//messageBox.className = "fixed";
+		messageBox.style.position = "absolute";
+		messageBox.style.top = "10px";
+		messageBox.style.left = "10px";
+		messageBox.style.zIndex = "-1000";
+		messageBox.style.width = "200px";
+		messageBox.style.height = "30px";
+		messageBox.style.borderRadius = "2px";
+		messageBox.style.display = "block";
+
+		var message = document.createElement("div");
+		messageBox.appendChild(message);
+		message.id = "message";
+
+		document.body.appendChild(messageBox);
+	}
+};
+
+//
+// <div id="messageBox" style="width:200px; height:30px;">
+// <div id="message" style="dispaly:none;"></div>
+// </div>
+$(function() {
+	window.Log._init();
+});
