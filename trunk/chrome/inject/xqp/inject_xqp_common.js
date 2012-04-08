68e4c86a65b6ff09bea9a550b139afb6d6b931d5
yaoancheng@gmail.com <yaoancheng@gmail.com@b5ad9fc9-fe54-84cf-8168-24ff7e8a642f>
2012-04-08 14:10:55 +0000

    需求协作平台
    修复AF流程无法显示系统名称和操作类型的问题
    非完美修复更多菜单，可以弹出菜单。遗留问题：无法自动关闭，需要点击一次更多关闭，鼠标移动没有效果。
    离线通讯录
    
    导出人员信息
    导出部门信息
    
    Portal
    新版Portal已使用新的侧栏工具条，同样有问题，已经修复

---
+++ trunk/chrome/inject/xqp/inject_xqp_common.js
@@ -0,0 +1,15 @@
+//优化AF单无法显示系统的问题
+//参考了http://blog.afterthedeadline.com/2010/05/14/how-to-jump-through-hoops-and-make-a-chrome-extension/
+(function() {
+	var scriptStr1 = '<script type="text/javascript">(function(l) {   var res = document.createElement("SCRIPT");  res.type = "text/javascript";   res.src = l;   document.getElementsByTagName("head")[0].appendChild(res);})("';
+	var scriptStr2 = '");</script>';
+	var url = chrome.extension.getURL("inject/xqp/inject_xqp_form.js");
+	jQuery('body').append(scriptStr1 + url + scriptStr2);
+
+	var url = window.location.href.toLowerCase();
+	if(url.indexOf("head.aspx") != -1) {
+		url = chrome.extension.getURL("inject/xqp/inject_xqp_head.js");
+		jQuery('body').append(scriptStr1 + url + scriptStr2);
+	}
+
+})();
