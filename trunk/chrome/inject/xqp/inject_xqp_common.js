46970d62d8da24fbd0a63326cf2b99875c05efbf
yaoancheng@gmail.com <yaoancheng@gmail.com@b5ad9fc9-fe54-84cf-8168-24ff7e8a642f>
2012-04-10 16:22:44 +0000

    完美解决需求平台AF单和更多菜单的问题

--- trunk/chrome/inject/xqp/inject_xqp_common.js
+++ trunk/chrome/inject/xqp/inject_xqp_common.js
@@ -4,7 +4,7 @@
 	var scriptStr1 = '<script type="text/javascript">(function(l) {   var res = document.createElement("SCRIPT");  res.type = "text/javascript";   res.src = l;   document.getElementsByTagName("head")[0].appendChild(res);})("';
 	var scriptStr2 = '");</script>';
 	var url = chrome.extension.getURL("inject/xqp/inject_xqp_form.js");
-	jQuery('body').append(scriptStr1 + url + scriptStr2);
+	jQuery('head').append(scriptStr1 + url + scriptStr2);
 
 	var url = window.location.href.toLowerCase();
 	if(url.indexOf("head.aspx") != -1) {
--- trunk/chrome/inject/xqp/inject_xqp_form.js
+++ trunk/chrome/inject/xqp/inject_xqp_form.js
@@ -1,4 +1,33 @@
 //AF单的脚本里，写了一句“null != document.frames.ifAttachment”，只有IE和Opera可以读取，因此此处为调整
-document.frames = {
-	ifAttachment : null
-};
+(function adjustTemplate() {
+	var ifAttachment = document.getElementById("ifAttachment");
+	if(ifAttachment) {
+		//如果存在表单，则定义之，是if可以进入
+		document.frames = {
+			ifAttachment : {
+				document : {
+					all : {
+						ucAttachment_trTemplateHeader : {
+							parentElement : null
+						}
+					}
+				}
+			}
+		};
+
+		/*
+		 在AF单界面中有这样的代码，如果用document.frame={ifAttachment : null}进行忽略，则无法产生template对象，也会让后面运行出错
+		 if (null != document.frames.ifAttachment) {
+		 var template = document.frames.ifAttachment.document.all.ucAttachment_trTemplateHeader.parentElement;
+		 template.style.display = "none";
+		 }*/
+		$(ifAttachment).load(function() {
+			document.frames.ifAttachment.document.all.ucAttachment_trTemplateHeader.parentElement = ifAttachment.contentWindow.document.getElementById("tbTemplate");
+		});
+	} else {
+		//否则，跳过if判断
+		document.frames = {
+			ifAttachment : null
+		};
+	}
+})();
