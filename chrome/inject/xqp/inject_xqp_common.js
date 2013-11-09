776fa959203bc5b5cadc86cebae09c248aba4b2f
yaoancheng@gmail.com <yaoancheng@gmail.com@b5ad9fc9-fe54-84cf-8168-24ff7e8a642f>
2013-11-09 07:31:48 +0000

    增加isite的辅助功能

--- chrome/inject/xqp/inject_xqp_common.js
+++ chrome/inject/xqp/inject_xqp_common.js
@@ -1,14 +1,23 @@
 //优化AF单无法显示系统的问题
 //参考了http://blog.afterthedeadline.com/2010/05/14/how-to-jump-through-hoops-and-make-a-chrome-extension/
 (function() {
-	var scriptStr1 = '<script type="text/javascript">(function(l) {   var res = document.createElement("SCRIPT");  res.type = "text/javascript";   res.src = l;   document.getElementsByTagName("head")[0].appendChild(res);})("';
-	var scriptStr2 = '");</script>';
+
+	// var scriptStr1 = '<script type="text/javascript">(function(l) {   var res = document.createElement("SCRIPT");  res.type = "text/javascript";   res.src = l;   document.getElementsByTagName("head")[0].appendChild(res);})("';
+	// var scriptStr2 = '");</script>';
+	// var url = chrome.extension.getURL("inject/xqp/inject_xqp_form.js");
+	// $('head').append(scriptStr1 + url + scriptStr2);
+	var s = document.createElement('script');
+	s.setAttribute('type', 'text/javascript');
 	var url = chrome.extension.getURL("inject/xqp/inject_xqp_form.js");
-	jQuery('head').append(scriptStr1 + url + scriptStr2);
+	s.setAttribute('src', url);
+	document.documentElement.appendChild(s);
 
 	var url = window.location.href.toLowerCase();
 	if(url.indexOf("head.aspx") != -1) {
 		url = chrome.extension.getURL("inject/xqp/inject_xqp_head.js");
-		jQuery('body').append(scriptStr1 + url + scriptStr2);
+		s = document.createElement('script');
+		s.setAttribute('type', 'text/javascript');
+		s.setAttribute('src', url);
+		document.documentElement.appendChild(s);
 	}
 })();
--- chrome/inject/xqp/inject_xqp_form.js
+++ chrome/inject/xqp/inject_xqp_form.js
@@ -1,5 +1,6 @@
 //AF单的脚本里，写了一句“null != document.frames.ifAttachment”，只有IE和Opera可以读取，因此此处为调整
 (function adjustTemplate() {
+	
 	var framesFunc = function(id) {
 		var f = document.getElementById(id);
 		if(f)
